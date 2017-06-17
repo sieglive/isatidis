2cf9e41cd81dcc87b171e7b4c5365f5a084f7322
Sieg Live <sieglive@126.com>
2017-06-17 17:17:40 +0800

    添加导航条

--- isatis-angular/src/app/app.module.ts
+++ isatis-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
