ec755becb1bb903a9adaa6c55d8726f60e0e247f
Sieg Live <sieglive@126.com>
2017-06-18 15:05:50 +0800

    添加router

--- isatis-angular/src/app/app.module.ts
+++ isatis-angular/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './comp/article/article.component';
+import { ProfileComponent } from './comp/profile/profile.component';
 
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+const appRoutes: Routes = [
+    { path: '', redirectTo: 'article', pathMatch: 'full' },
+    { path: 'article', component: ArticleComponent },
+    { path: 'profile', component: ProfileComponent },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    // NgbModule.forRoot(),
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ArticleComponent,
+        ProfileComponent
+    ],
+    imports: [
+        // NgbModule.forRoot(),
+        RouterModule.forRoot(appRoutes),
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
