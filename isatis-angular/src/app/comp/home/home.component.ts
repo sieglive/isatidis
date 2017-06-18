405ddd8607ccf8aa144a061266d7bd87fee3e017
SL-DESKTOPPC\siegl <sieglive@126.com>
2017-06-18 21:47:43 +0800

    添加输入，鼠标响应，回车响应。

--- isatis-angular/src/app/comp/home/home.component.ts
+++ isatis-angular/src/app/comp/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  title = 'app';
+    @Input() title: String;
+    selectTitle = this.title;
+
+    onSelect(title: String): void {
+        this.selectTitle = this.title;
+    }
+
+    update_title_on_key(event: any, title: String) {
+        if (event.key == "Enter") {
+            console.log(event);
+            this.selectTitle = title;
+        }
+    }
+
+    goBack(title: String): void {
+        this.selectTitle = title;
+        console.log(title);
+    }
 }
