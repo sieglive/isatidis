34eba8b28807dc042f2098523fd811c7e698e0c1
Cao Jiahang <caojh@wondershare.cn>
2017-06-19 13:43:14 +0800

    更换md风格

--- isatis-angular/src/app/app.module.ts
+++ isatis-angular/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './comp/home/home.component'
 import { ArticleComponent } from './comp/article/article.component';
 import { ProfileComponent } from './comp/profile/profile.component';
-
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialModule } from '@angular/material';
 const appRoutes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
@@ -24,9 +23,13 @@ const appRoutes: Routes = [
         ProfileComponent
     ],
     imports: [
-        // NgbModule.forRoot(),
-        RouterModule.forRoot(appRoutes),
-        BrowserModule
+        MaterialModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        // MdButtonModule,
+        // MdCheckboxModule,
+        // MdInputModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [],
     bootstrap: [AppComponent]
