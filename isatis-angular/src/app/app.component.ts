49f5e43dabaf6ded0fa83626a2a2f0b82c49ffcc
SL-DESKTOPPC\siegl <sieglive@126.com>
2017-07-15 09:47:56 +0800

    添加注册功能

--- isatis-angular/src/app/app.component.ts
+++ isatis-angular/src/app/app.component.ts
@@ -1,41 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { WindowRef } from 'app/service/window.service';
+import { Component, OnInit, Directive, ViewChild, AfterViewInit } from '@angular/core';
+// import { WindowRef } from 'app/service/window.service';
 // import { BackgroundService } from 'app/background.service';
+
+
 @Component({
     selector: 'isatis-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
     private disabled_background = false;
-    private toggle = false;
-    constructor(private winRef: WindowRef) { }
+    // private toggle = false;
+    constructor() { }
 
     ngOnInit() {
-        if (this.toggle) {
-            this.winRef.nativeWindow.particlesJS.load('particles-js', 'assets/particles.json', function () {
-                console.log('callback - particles.js config loaded');
-            });
-        }
+        // if (this.toggle) {
+        //     this.winRef.nativeWindow.particlesJS.load('particles-js', 'assets/particles.json', function () {
+        //         console.log('callback - particles.js config loaded');
+        //     });
+        // }
     }
 
-    change_bg() {
-        if (this.toggle) {
-            this.winRef.nativeWindow.pJSDom = [];
-            this.winRef.nativeWindow.particlesJS.load('particles-js', 'assets/particles.json', function () {
-                console.log('callback - particles.js config loaded');
-            });
-        } else {
-            this.winRef.nativeWindow.pJSDom[0].pJS.interactivity.events.onclick.enable = false;
-            this.winRef.nativeWindow.pJSDom[0].pJS.interactivity.events.onhover.enable = false;
-            this.winRef.nativeWindow.pJSDom[0].pJS.particles.array = [];
-            this.winRef.nativeWindow.pJSDom[0].pJS.particles.move.enable = false;
-        }
+    ngAfterViewInit() {
     }
 
-    clean_bg() {
-        this.winRef.nativeWindow.pJSDom[0].pJS.particles.array = [];
-    }
+    // change_bg() {
+    //     if (this.toggle) {
+    //         this.winRef.nativeWindow.pJSDom = [];
+    //         this.winRef.nativeWindow.particlesJS.load('particles-js', 'assets/particles.json', function () {
+    //             console.log('callback - particles.js config loaded');
+    //         });
+    //     } else {
+    //         this.winRef.nativeWindow.pJSDom[0].pJS.interactivity.events.onclick.enable = false;
+    //         this.winRef.nativeWindow.pJSDom[0].pJS.interactivity.events.onhover.enable = false;
+    //         this.winRef.nativeWindow.pJSDom[0].pJS.particles.array = [];
+    //         this.winRef.nativeWindow.pJSDom[0].pJS.particles.move.enable = false;
+    //     }
+    // }
+
+    // clean_bg() {
+    //     this.winRef.nativeWindow.pJSDom[0].pJS.particles.array = [];
+    // }
 
 }
--- isatis-angular/src/app/comp/user/user_auth.component.ts
+++ isatis-angular/src/app/comp/user/user_auth.component.ts
@@ -8,11 +8,12 @@ import { IsatisHttp } from 'app/service/isatis_http.service';
     styleUrls: ['./user_auth.component.scss']
 })
 export class UserAuthComponent {
-    private user_email: string;
-    private user_password: string;
-    private resObject: object;
-    private res: object;
-    private user_time: number;
+    public user_email = '';
+    public user_password = '';
+    public user_account = '';
+    public user_nick = '';
+    public result: any;
+    public user_time: number;
 
     constructor(private _http: IsatisHttp) { }
 
@@ -20,22 +21,27 @@ export class UserAuthComponent {
     }
 
     signIn() {
-        this._http.get(
-            '/middle/account/login',
-            { email: this.user_email, password: this.user_password },
-            res => { const body = res.json(); return body },
-            res => { this.user_time = res.time });
+        const result = this._http.post(
+            '/back/account/auth',
+            {
+                email: this.user_email,
+                user_pass: this.user_password
+            }
+        );
+        result.subscribe(res => { this.user_time = res.json().time; });
     }
-    // signUp() {
-    //     this._http.res = {
-    //         url: '/middle/account/login',
-    //         params: { email: this.user_email, password: this.user_password },
-    //         callback: res => {
-    //             const body = res.json();
-    //             return body;
-    //         }
-    //     }
-    //     console.log(this._http.res);
 
-    // }
+    signUp() {
+        const result = this._http.put(
+            '/back/account/auth',
+            {
+                email: this.user_email,
+                user_pass: this.user_password,
+                nick_name: this.user_nick,
+                user_name: this.user_account
+            }
+        );
+        console.log(this.user_nick);
+        result.subscribe(res => { this.user_time = res.json().time; });
+    }
 }
