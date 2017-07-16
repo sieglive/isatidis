2ab1f8afa195b9a23603f99d702fa03bcaca3706
SL-DESKTOPPC\siegl <sieglive@126.com>
2017-07-16 22:53:43 +0800

    修改profile 样式

--- isatis-angular/src/app/comp/profile/profile.component.ts
+++ isatis-angular/src/app/comp/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, DoCheck } from '@angular/core';
 
 @Component({
-  selector: 'isatis-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'isatis-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-  title = 'app';
+export class ProfileComponent implements OnChanges, DoCheck {
+    public user_email = '';
+    public user_pass = '';
+    public user_name = '';
+    public user_nick = '';
+    public properties = [
+        { name: 'user_nick', value: this.user_nick },
+        { name: 'user_pass', value: this.user_pass },
+        { name: 'user_name', value: this.user_name },
+        { name: 'user_email', value: this.user_email }
+    ]
+
+    ngOnChanges(change) {
+        console.log(change);
+        console.log(this.user_nick, this.user_pass, this.user_name, this.user_email);
+    }
+
+
+    ngDoCheck() {
+        console.log(this.properties);
+    }
 }
