061fe06a1d40054a827f3249628a0dc143590d0e
Cao Jiahang <caojh@wondershare.cn>
2017-07-28 18:12:18 +0800

    样式修改

--- isatis-angular/src/app/comp/bulletin/bulletin.component.ts
+++ isatis-angular/src/app/comp/bulletin/bulletin.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'isatis-bulletin',
-  templateUrl: './bulletin.component.html',
-  styleUrls: ['./bulletin.component.scss']
+    selector: 'isatis-bulletin',
+    templateUrl: './bulletin.component.html',
+    styleUrls: ['./bulletin.component.scss']
 })
 export class BulletinComponent {
-  title = 'app';
+    public tip_list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    tipListEmpty() {
+    }
+
+    addCard() {
+
+    }
 }
